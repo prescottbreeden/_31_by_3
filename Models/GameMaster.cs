76b6db1c847e4dfa0ad75e6538abded1c4422bf4
prescott breeden <pb@prescotts-iMac.local>
2018-04-22 00:44:17 -0700

    Tweaked AI, added AI knocking
    all functioning

--- Models/GameMaster.cs
+++ Models/GameMaster.cs
@@ -6,6 +6,7 @@ namespace _31_by_3
     {
         public int turn { get; set; }
         public Deck deck { get; set; }
+        public bool knocked { get; set; }
         public List<Player> players { get; set; }
         public GameOver endGame { get; set; }
         public GameMaster()
